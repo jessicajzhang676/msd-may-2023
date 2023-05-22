c44e9957e73b98968eba378c1cfd1a9b3a0a4649
Jessica Zhang <jzhang2@progressive.com>
2023-05-22 13:45:17 -0400

    Initial changes

---
+++ src/JobSolution/JobsApi/Controllers/UniqueIdChecker.cs
@@ -0,0 +1,13 @@
+﻿using SlugGenerators;
+
+namespace JobsApi.Controllers;
+
+public class UniqueIdChecker : ICheckForUniqueValues
+
+{
+    public Task<bool> IsUniqueAsync(string attempt)
+    {
+        // TODO: check the database, make sure it isn't used...
+        return Task.FromResult(true);
+    }
+}
